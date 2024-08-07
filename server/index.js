ef277e2fc7de706f1559a92286da9cbb9f052883
ErnestoDanielOrtiz <dohuerta@gmail.com>
2024-08-07 14:51:42 -0600

    Connection with database in mysql

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const mysql = require('mysql');
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+//Creates connection to local database
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: '',
+  database: 'PasswordManager',
+});
 
+//Test route for API
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+//Test if server is running
 app.listen(PORT, () => {
-    console.log('Server running');
-})
+  console.log('Server running');
+});
