632011c23b4a6afbb6f3113e3f4dff00054838b3
ErnestoDanielOrtiz <dohuerta@gmail.com>
2024-08-12 12:13:00 -0600

    Created simple form to add a website and password

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="AddingPassword">
+        <input type="text" placeholder="Ex. Google" />
+        <input type="text" placeholder="Ex. password123" />
+        <button>Add Password</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
