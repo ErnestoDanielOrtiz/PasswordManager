0340e695796ba53a30dfce82e6000eaddbf5a0c4
ErnestoDanielOrtiz <dohuerta@gmail.com>
2024-08-13 11:32:03 -0600

    Created states for Title and Password

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [title, setTitle] = useState('');
+
   return (
     <div className="App">
       <div className="AddingPassword">
-        <input type="text" placeholder="Ex. Google" />
-        <input type="text" placeholder="Ex. password123" />
+        <input
+          type="text"
+          placeholder="Ex. Google"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Ex. password123"
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
         <button>Add Password</button>
       </div>
     </div>
